0bf81275c77ea2bdeebfb1fc6ac3cc49a6f27653
Sven Schuett <hsven@me.com>
2022-07-31 14:59:33 +0200

    Version without Test Branch

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hello")
         print("This is a great Branch")
+        print("Version without Test Branch")
         // Do any additional setup after loading the view.
     }
     
