30a6dcbdcd422373299c6b7bc65b32985c1abd18
Sven Schuett <hsven@me.com>
2022-07-31 14:41:07 +0200

    Added Print 'This is a Test Branch'

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("This is a Test Branch")
         // Do any additional setup after loading the view.
     }
     
